98c2603c7c9d62b8fcf6d3b779b1057c625a4a4d
Tom Shilliday <tshilliday@gmail.com>
2025-04-19 10:38:27 +0100

    fix: move theme creation to separate client component

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,29 +1,8 @@
 "use client";
 
-import {
-  ThemeProvider as MuiThemeProvider,
-  createTheme,
-} from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
 import { ReactNode } from "react";
-
-const theme = createTheme({
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#1976d2",
-    },
-    secondary: {
-      main: "#dc004e",
-    },
-  },
-});
+import ThemeWrapper from "./ThemeWrapper";
 
 export default function ThemeProvider({ children }: { children: ReactNode }) {
-  return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </MuiThemeProvider>
-  );
+  return <ThemeWrapper>{children}</ThemeWrapper>;
 }
